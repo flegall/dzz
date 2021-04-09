81b63d61f0c22a4b4335ecbd69a87fa924d9bc24
Florent Le Gall <florent.legall@gmail.com>
2021-04-09 08:42:00 +0000

    Add new examples

---
+++ src/bin/println.rs
@@ -0,0 +1,225 @@
+// Import (via `use`) the `fmt` module to make it available.
+use std::fmt;
+
+fn main() {
+    println!("{} days", 31);
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+    // As can named arguments.
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "The quick brown fox",
+        verb = "jumps over"
+    );
+
+    // Special formatting can be specified after a `:`.
+    println!(
+        "{} of {:b} people know binary, the other half doesn't",
+        1, 2
+    );
+
+    // You can right-align text with a specified width. This will output
+    // "     1". 5 white spaces and a "1".
+    println!("{number:>width$}", number = 1, width = 6);
+
+    // You can pad numbers with extra zeroes. This will output "000001".
+    println!("{number:>0width$}", number = 1, width = 6);
+
+    // Rust even checks to make sure the correct number of arguments are
+    // used.
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+    // FIXME ^ Add the missing argument: "James"
+
+    // Create a structure named `Structure` which contains an `i32`.
+    // #[allow(dead_code)]
+    // struct Structure(i32);
+
+    // However, custom types such as this structure require more complicated
+    // handling. This will not work.
+    // println!("This struct `{}` won't print...", Structure(3));
+    // FIXME ^ Comment out this line.
+
+    let pi = 3.141592;
+    println!("PI is roughly {pi:.3}", pi = pi);
+
+    // Derive the `fmt::Debug` implementation for `Structure`. `Structure`
+    // is a structure which contains a single `i32`.
+    #[derive(Debug)]
+    struct Structure(i32);
+
+    // Put a `Structure` inside of the structure `Deep`. Make it printable
+    // also.
+    #[derive(Debug)]
+    struct Deep(Structure);
+
+    // `Structure` is printable with :? formatter
+    println!("Now {:?} will print!", Structure(3));
+    // Deep too
+    println!("Now {:?} will print!", Deep(Structure(7)));
+
+    #[derive(Debug)]
+    struct Person<'a> {
+        name: &'a str,
+        age: u8,
+    }
+
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Pretty print with #? formatter
+    println!("{:#?}", peter);
+
+    impl fmt::Display for Structure {
+        // This trait requires `fmt` with this exact signature.
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // Write strictly the first element into the supplied output
+            // stream: `f`. Returns `fmt::Result` which indicates whether the
+            // operation succeeded or failed. Note that `write!` uses syntax which
+            // is very similar to `println!`.
+            write!(f, "THE VALUE IS {}", self.0)
+        }
+    }
+
+    println!("This struct now prints `{}` :)", Structure(3));
+
+    #[derive(Debug)]
+    struct Complex {
+        real: f64,
+        imag: f64,
+    }
+    impl fmt::Display for Complex {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{real} + {imag}i", real = self.real, imag = self.imag)
+        }
+    }
+
+    let complex = Complex {
+        real: 3.3,
+        imag: 7.2,
+    };
+    println!("Display: {}", complex);
+    println!("Debug: {:?}", complex);
+
+    // Define a structure named `List` containing a `Vec`.
+    struct List(Vec<i32>);
+
+    impl fmt::Display for List {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // Extract the value using tuple indexing,
+            // and create a reference to `vec`.
+            let vec = &self.0;
+
+            write!(f, "[")?;
+
+            // Iterate over `v` in `vec` while enumerating the iteration
+            // count in `count`.
+            for (count, v) in vec.iter().enumerate() {
+                // For every element except the first, add a comma.
+                // Use the ? operator to return on errors.
+                if count != 0 {
+                    write!(f, ", ")?;
+                }
+                write!(f, "{count}: {v}", count = count, v = v)?;
+            }
+
+            // Close the opened bracket and return a fmt::Result value.
+            write!(f, "]")
+        }
+    }
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+
+    struct City {
+        name: &'static str,
+        // Latitude
+        lat: f32,
+        // Longitude
+        lon: f32,
+    }
+
+    impl fmt::Display for City {
+        // `f` is a buffer, and this method must write the formatted string into it
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+            let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+            // `write!` is like `format!`, but it will write the formatted string
+            // into a buffer (the first argument)
+            write!(
+                f,
+                "{}: {:.3}°{} {:.3}°{}",
+                self.name,
+                self.lat.abs(),
+                lat_c,
+                self.lon.abs(),
+                lon_c
+            )
+        }
+    }
+
+    #[derive(Debug)]
+    struct Color {
+        red: u8,
+        green: u8,
+        blue: u8,
+    }
+    impl fmt::Display for Color {
+        // `f` is a buffer, and this method must write the formatted string into it
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(
+                f,
+                "RGB ({red}, {green}, {blue}) 0x{red:02X?}{green:02X?}{blue:02X?}",
+                red = self.red,
+                green = self.green,
+                blue = self.blue,
+            )
+        }
+    }
+
+    for city in [
+        City {
+            name: "Dublin",
+            lat: 53.347778,
+            lon: -6.259722,
+        },
+        City {
+            name: "Oslo",
+            lat: 59.95,
+            lon: 10.75,
+        },
+        City {
+            name: "Vancouver",
+            lat: 49.25,
+            lon: -123.1,
+        },
+    ]
+    .iter()
+    {
+        println!("{}", city);
+    }
+    for color in [
+        Color {
+            red: 128,
+            green: 255,
+            blue: 90,
+        },
+        Color {
+            red: 0,
+            green: 3,
+            blue: 254,
+        },
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0,
+        },
+    ]
+    .iter()
+    {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display.
+        println!("{}", color);
+    }
+}
